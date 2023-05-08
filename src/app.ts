0c5a30efb5700579f4721438807113defaa0c6d5
Leonardo Vitale <leonardo.vitale@outlook.com>
2023-05-08 18:25:33 +0100

    chore: install prisma

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
+import { Prisma, PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Alice',
+    email: 'teste@teste.colm',
+  },
+})
 
 export default app
