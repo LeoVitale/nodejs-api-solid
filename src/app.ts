5001c94c04674424932f32b755c51b30c1269cf0
Leonardo Vitale <leonardo.vitale@outlook.com>
2023-05-08 22:11:49 +0100

    refac:improve error handling

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,30 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      details: error.format(),
+      issues: error.issues,
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Send error to Sentry/LogRocket/Loggly/
+  }
+
+  return reply.status(500).send({
+    message: 'Internal server error',
+  })
+})
 
 export default app
--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists'
 import { RegisterUseCase } from '@/use-cases/register'
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
@@ -21,7 +22,12 @@ export const register = async (
 
     await registerUseCase.execute({ name, email, password })
   } catch (error) {
-    return reply.status(409).send()
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({
+        message: error.message,
+      })
+    }
+    return reply.status(500).send()
   }
 
   return reply.status(201).send()
